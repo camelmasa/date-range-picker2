4ca154de8d3257309a8682638b97f003aa3769bb
camelmasa <camelmasa@gmail.com>
2018-11-02 02:34:12 +0900

    feat: multi locale

---
+++ src/locale/index.js
@@ -0,0 +1,9 @@
+import en from "./en.js"
+import ja from "./ja.js"
+
+const locales = {
+  en: en,
+  ja: ja
+}
+
+export default locales
---
+++ src/locale/ja.js
@@ -0,0 +1,14 @@
+const locale = {
+  name: "ja",
+  start: "開始",
+  startFormat: "YYYY年MM月DD日",
+  end: "終わり",
+  endFormat: "YYYY年MM月DD日",
+  yearFormat: 'YYYY年',
+  monthFormat: 'M月',
+  dayOfWeek: "日_月_火_水_木_金_土".split('_'),
+  cancel: "キャンセル",
+  submit: "決定"
+}
+
+export default locale
